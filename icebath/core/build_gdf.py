a86e1c57e3ca011eaed9da7e1f3e37b038082e2d
Jessica Scheick <jbscheick@gmail.com>
2020-11-19 16:03:58 -0500

    add notes about processing

--- icebath/core/build_gdf.py
+++ icebath/core/build_gdf.py
@@ -21,6 +21,8 @@ def xarray_to_gdf(xr):
         berg_gdf = berg_gdf.append(temp_berg_df, ignore_index=True)
 
     berg_gdf.crs = xr.attrs['crs']
+    berg_gdf.sl_adjust.attrs['note'] = "sea level adjustment is relative to tidal height, not 0msl"
+    berg_gdf.sl_adjust.attrs['units'] = "meters"
 
     return berg_gdf
 
@@ -32,9 +34,9 @@ def gdf_of_bergs(onedem):
     # np.warnings.filterwarnings('error', category=np.VisibleDeprecationWarning)
     
     bergs = onedem['berg_outlines'].item()
-    elevs=np.empty_like(bergs, dtype=object)
+    elevs = np.empty(len(bergs), dtype=object)
     sl_adj=np.zeros(len(bergs))
-    berg_poly=np.empty_like(bergs, dtype=object)
+    berg_poly = np.empty(len(bergs), dtype=object)
 
 
     i=0
@@ -57,21 +59,41 @@ def gdf_of_bergs(onedem):
             continue
         # remove nans because causing all kinds of issues down the processing pipeline (returning nan as a result and converting entire array to nan)
         vals = vals[~np.isnan(vals)]
-        
+
         elevs[i] = vals[vals>=onedem.attrs['berg_threshold']]
         # get the regional elevation values and determine the sea level adjustment
         #TODO: make this border/boundary larger than one pixel (and do it by number of pixels!)
         bvals = onedem['elevation'].sel(x=slice(bound_box[0]-100, bound_box[2]+100),
                                         y=slice(bound_box[1]-100, bound_box[3]+100)).values.flatten()
-        # print(bvals)
         bvals=bvals[~np.isnan(bvals)]
         sea = bvals[bvals<onedem.attrs['berg_threshold']]
         # print(sea)
+        # NOTE: sea level adjustment (m) is relative to tidal height at the time of image acquisition, not 0 msl
         sl_adj[i] = np.nanmedian(sea)
         # print(sl_adj[i])
         #add a check here to make sure the sea level adjustment is reasonable
+
         # print('check for a reasonable sea level adjustment')
-        
+        '''
+        sea level adjustment uncertainty from matlab (likely need to implement here)
+        sl_dz1 = icefree_area1(~isnan(icefree_area1)); sl_dz1(sl_dz1>nanmedian(sl_dz1)+2*2.9 | sl_dz1<nanmedian(sl_dz1)-2*2.9) = NaN; %I think this is removing 2 sigma outliers, but I'm not sure what the significance of the 2.9 is (sigma calculated when/how?)
+        '''
+
+        '''
+
+        lines 756-763
+        %calculate some basic info about the iceberg polygon/DEM data coverage
+        %%non_nans = ~isnan(IB.zo.values.map);
+        %%nans = isnan(IB.zo.values.map);
+        IB.zo.cov.numpix = sum(sum(IB.berg_mask));
+        IB.zo.cov.numpix_nan = sum(sum(IB.berg_mask.*isnan(IB.zo.values.map)));
+        IB.zo.cov.area = IB.zo.cov.numpix * DEM1_pixel_area; %=base_area, below
+        IB.zo.cov.area_nan = IB.zo.cov.numpix_nan * DEM1_pixel_area;
+        IB.zo.cov.percent = IB.zo.cov.area_nan/IB.zo.cov.area*100;
+
+        '''
+
+
         #apply the sea level adjustment to the elevation values
         elevs[i] = icalcs.apply_decrease_offset(elevs[i], sl_adj[i])
 
--- icebath/utils/raster_ops.py
+++ icebath/utils/raster_ops.py
@@ -54,11 +54,14 @@ def poly_from_thresh(x,y,elev,threshold):
 
     '''
 
-    elev = np.nan_to_num(elev, nan=0)
+    elev = elev#np.nan_to_num(elev, nan=0)
     X,Y = np.meshgrid(x,y)
     fig = plt.figure()
     cs = plt.contour(X, Y, elev, 'k-', levels=[threshold])
     # Can use collections[0] so long as you only have one threshold value. Otherwise you have to iterate through the collections.
+    # print(cs.collections[0].get_alpha())
+    # cs.collections.get_alpha()
+    print(len(cs.collections))
     p = cs.collections[0].get_paths()
     polygons = []
     
@@ -73,6 +76,9 @@ def poly_from_thresh(x,y,elev,threshold):
     # It is also recommended to now use scikit-image for contour finding (skimage.measure.find_contours)
 
     for contour_path in p: 
+        # try to get the alpha value or otherwise determine if the polygon traces a nan region
+        # print(contour_path.get_alpha)
+
         # The first polygon in the path is the main one, the following ones are "holes"
         for ncp,cp in enumerate(contour_path.to_polygons(closed_only=False)):
             if ncp>0:
@@ -82,8 +88,9 @@ def poly_from_thresh(x,y,elev,threshold):
             if cp[-1,0] == cp[0,0] and cp[-1,1] == cp[0,1]:
                 polygons.append(cp)
 
-    # print(len(polygons))
-    # print(type(polygons))
+
+    print(len(polygons))
+    print(type(polygons))
     plt.close(fig)
 
     return polygons
